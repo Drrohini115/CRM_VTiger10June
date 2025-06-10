eea66b7dc57cf2b526b7a7f4f390414381eebf42
Rohini <Rohini@Rohini>
2025-06-10 13:03:02 +0530

    commitis added

--- com.comtekpyramid.aircooler/src/main/java/com/demo/Login/DemoTest.java
+++ com.comtekpyramid.aircooler/src/main/java/com/demo/Login/DemoTest.java
@@ -1,11 +1,14 @@
 package com.demo.Login;
 import org.testng.annotations.Test;
-
+/**
+ * @author Rohini
+ * i created the demo
+ */
 
 public class DemoTest {
 @Test
 public void loginAction() {
 	System.out.println("done");
 }
-
+//as per leads review demo is updated
 }
